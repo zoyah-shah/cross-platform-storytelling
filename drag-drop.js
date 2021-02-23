038f64cd3051759244edec5686740a4e77072761
zoyah-shah <70984151+zoyah-shah@users.noreply.github.com>
2021-02-23 13:48:46 -0500

    made home page, question page, and exclamation page

---
+++ drag-drop.js
@@ -0,0 +1,129 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
+  function allowDrop2(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag2(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop2(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
+  function allowDrop3(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag3(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop3(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
+
+  function allowDrop4(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag4(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop4(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
+  function allowDrop5(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag5(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop5(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
+
+  function allowDrop6(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag6(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop6(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
+
+  function allowDrop7(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag7(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop7(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
+
+  function allowDrop8(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag8(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop8(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
